cac2f3965b5d9203d666c1abc3c4c55712ab191e
Shreedarsshini19 <197891326+Shreedarsshini19@users.noreply.github.com>
2026-04-25 22:14:02 +0530

    Base application setup

---
+++ src/org/example/TrainConsistManagementApp.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===\n");
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train consist initialized successfully.");
+        System.out.println("Initial bogie count: " + trainConsist.size());
+
+        System.out.println("\nProgram is ready for further operations...");
+    }
+}
